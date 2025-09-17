93e80d5ed4c496d2714ec34f34b71ebcf2a108be
sebastian-gfrd <sc.martineza12@uniandes.edu.co>
2025-09-17 08:15:27 -0500

    Punto 1

--- src/main/java/co/edu/uniandes/dse/parcial1/entities/MercanciaEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/MercanciaEntity.java
@@ -1,12 +1,20 @@
 package co.edu.uniandes.dse.parcial1.entities;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Data
 @Entity
 public class MercanciaEntity extends BaseEntity{
-   
-    
-
+   private String nombre;
+   @Id
+   private String codigo_de_barras;
+   private LocalDateTime fecha_de_recepcion;
+   private Integer cantidad_disponible;
+   @OneToOne
+   private UbicacionBodegaEntity ubicacion;
 }
--- src/main/java/co/edu/uniandes/dse/parcial1/entities/UbicacionBodegaEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/UbicacionBodegaEntity.java
@@ -1,11 +1,19 @@
 package co.edu.uniandes.dse.parcial1.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
 @Entity
 public class UbicacionBodegaEntity extends BaseEntity{
-   
-    
-}
+    @Id
+    private Integer numero_de_estante;
+    @OneToMany
+    private List<MercanciaEntity> canasta = new ArrayList<MercanciaEntity>();
+    private Integer peso_maximo;
+}   
